3e80c0dfcda42eeb802146268b06f5902520e0e8
kopyl <oleggggg1997@gmail.com>
2024-02-16 13:22:08 +0200

    Refactor nav

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -3,29 +3,36 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+const MenuLink = ({ href, styles, children }: any) => {
+  const pathname = usePathname();
+  return (
+    <Link
+      href={href}
+      passHref
+      className={`${styles.navLink} ${
+        `${pathname}` === href ? styles.active : ""
+      }`}
+    >
+      {children}
+    </Link>
+  );
+};
+
 export const Nav = ({ styles }: any) => {
   const pathname = usePathname();
-
-  const MenuLink = ({ href, children }: any) => {
-    return (
-      <Link
-        href={href}
-        passHref
-        className={`${styles.navLink} ${
-          `${pathname}` === href ? styles.active : ""
-        }`}
-      >
-        {children}
-      </Link>
-    );
-  };
 
   return (
     <nav className={styles.nav}>
       <div className={styles.navList}>
-        <MenuLink href="/#models">What models do I work with</MenuLink>
-        <MenuLink href="/clients">Trusted by</MenuLink>
-        <MenuLink href="/#stack">My stack</MenuLink>
+        <MenuLink href="/#models" styles={styles}>
+          What models do I work with
+        </MenuLink>
+        <MenuLink href="/clients" styles={styles}>
+          Trusted by
+        </MenuLink>
+        <MenuLink href="/#stack" styles={styles}>
+          My stack
+        </MenuLink>
       </div>
     </nav>
   );
