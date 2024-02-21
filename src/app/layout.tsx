6ded4b2e91edf7bc735fac872b78af27f8df7502
kopyl <oleggggg1997@gmail.com>
2024-02-21 05:20:23 +0200

    Refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className="header">
-          <Header />
-        </header>
+        <Header />
         {children}
         <Promo />
         <Footer />
