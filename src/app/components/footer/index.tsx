21d4212d51cfebf9be472a1afb80606467ab75ff
kopyl <oleggggg1997@gmail.com>
2024-02-21 07:37:22 +0200

    Make footer

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -27,6 +27,33 @@ export const Footer = ({ children }: any) => {
           </div>
           <span className={styles.info}>I’m not a RunPod employee</span>
         </CenteredContainer>
+        <CenteredContainer className={styles.footerMobileContainer}>
+          <div className={styles.links}>
+            <div className={styles.linksColumn}>
+              <Link href="/contacts" className={styles.link}>
+                LinkedIn
+              </Link>
+              <Link href="/privacy" className={styles.link}>
+                WhatsApp
+              </Link>
+              <Link href="/terms" className={styles.link}>
+                Telegram
+              </Link>
+            </div>
+            <div className={styles.linksColumn}>
+              <Link href="/terms" className={styles.link}>
+                Discord
+              </Link>
+              <Link href="/terms" className={styles.link}>
+                Email
+              </Link>
+            </div>
+          </div>
+          <div className={styles.infoColumn}>
+            <span className={styles.info}>I’m not a RunPod employee</span>
+            <span className={styles.info}>Designed & developed myself</span>
+          </div>
+        </CenteredContainer>
       </SideSafeArea>
     </div>
   );
