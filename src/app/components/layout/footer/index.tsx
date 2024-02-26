b9888bcd04c49f297bf1cffbedfe8772e5fe3be2
kopyl <oleggggg1997@gmail.com>
2024-02-26 05:27:40 +0200

    Avoid duplicating the same links

--- src/app/components/layout/footer/index.tsx
+++ src/app/components/layout/footer/index.tsx
@@ -2,6 +2,18 @@ import styles from "./footer.module.sass";
 import { CenteredContainer, SideSafeArea } from "@components/util/util";
 import Link from "next/link";
 
+const links = [
+  [
+    { title: "LinkedIn", href: "/contacts" },
+    { title: "WhatsApp", href: "/privacy" },
+    { title: "Telegram", href: "/terms" },
+  ],
+  [
+    { title: "Discord", href: "/terms" },
+    { title: "Email", href: "/terms" },
+  ],
+];
+
 export const Footer = () => {
   return (
     <div className={styles.footer}>
@@ -9,44 +21,29 @@ export const Footer = () => {
         <CenteredContainer className={styles.footerDesktopContainer}>
           <span className={styles.info}>Designed & developed myself</span>
           <div className={styles.links}>
-            <Link href="/contacts" className={styles.link}>
-              LinkedIn
-            </Link>
-            <Link href="/privacy" className={styles.link}>
-              WhatsApp
-            </Link>
-            <Link href="/terms" className={styles.link}>
-              Telegram
-            </Link>
-            <Link href="/terms" className={styles.link}>
-              Discord
-            </Link>
-            <Link href="/terms" className={styles.link}>
-              Email
-            </Link>
+            {links.flat().map((link, index) => (
+              <Link href={link.href} key={index} className={styles.link}>
+                {link.title}
+              </Link>
+            ))}
           </div>
           <span className={styles.info}>I’m not a RunPod employee</span>
         </CenteredContainer>
         <CenteredContainer className={styles.footerMobileContainer}>
           <div className={styles.links}>
             <div className={styles.linksColumn}>
-              <Link href="/contacts" className={styles.link}>
-                LinkedIn
-              </Link>
-              <Link href="/privacy" className={styles.link}>
-                WhatsApp
-              </Link>
-              <Link href="/terms" className={styles.link}>
-                Telegram
-              </Link>
+              {links[0].map((link, index) => (
+                <Link href={link.href} key={index} className={styles.link}>
+                  {link.title}
+                </Link>
+              ))}
             </div>
             <div className={styles.linksColumn}>
-              <Link href="/terms" className={styles.link}>
-                Discord
-              </Link>
-              <Link href="/terms" className={styles.link}>
-                Email
-              </Link>
+              {links[1].map((link, index) => (
+                <Link href={link.href} key={index} className={styles.link}>
+                  {link.title}
+                </Link>
+              ))}
             </div>
           </div>
           <div className={styles.infoColumn}>
