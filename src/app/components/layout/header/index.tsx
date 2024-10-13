1dcd1ab1e831dfac03097c0efba409c32a77922f
kopyl <oleggggg1997@gmail.com>
2024-10-14 02:25:00 +0300

    Hide mobile greeting on scroll

--- src/app/components/layout/header/index.tsx
+++ src/app/components/layout/header/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Nav } from "../../nav";
 import styles from "./header.module.sass";
 import Link from "next/link";
@@ -5,8 +6,30 @@ import Image from "next/image";
 import { SecondaryButton } from "@/app/components/buttons";
 import { CenteredContainer, SideSafeArea } from "@components/util/util";
 import { Menu } from "@components/layout/menu";
+import { useState, useEffect } from "react";
 
 export const Header = ({ searchParams }: any) => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    let position = window.pageYOffset;
+    if (position > 10) {
+      position = 55;
+    } else {
+      position = 0;
+    }
+    setScrollPosition(position);
+    console.log(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll); // Clean up on unmount
+    };
+  }, []);
+
   return (
     <div className={styles.header}>
       <Menu menuOpen={searchParams.isMenuOpen === "true"} />
@@ -17,7 +40,12 @@ export const Header = ({ searchParams }: any) => {
             <p className={styles.type}>Kopyl Oleh</p>
           </Link>
           <Nav styles={styles} />
-          <p className={styles.heroGreeting}>Hi I’m a AI engineer 👋</p>
+          <p
+            className={styles.heroGreeting}
+            style={{ position: "relative", top: `-${scrollPosition}px` }}
+          >
+            Hi I’m a AI engineer 👋
+          </p>
           <SecondaryButton
             className={styles.hireButton}
             href="https://docs.google.com/document/d/1RuSI_f3PZuqhJNZhgkKXx0V-BCQ-V2p2Hy4s4A1pwKQ"
